85484f8adc3e11f773db860e96a999a8a3311185
halyrds1-alt <halyrds1-alt@users.noreply.github.com>
2026-08-04 11:47:29 +0300

    InfoGram v2.4 - Fixed crash, replaced all Telegram logos with custom logo

--- TMessagesProj/src/main/java/org/telegram/ui/IntroActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/IntroActivity.java
@@ -153,6 +153,8 @@ public class IntroActivity extends BaseFragment implements NotificationCenter.No
 
     @Override
     public View createView(Context context) {
+        logoDrawable = context.getResources().getDrawable(R.drawable.telegram_logo).mutate();
+        logoDrawable.setBounds(0, dp(8.666f), dp(115), dp(35));
         titles[0] = LocaleController.getString(R.string.Page1Title);
 
 
