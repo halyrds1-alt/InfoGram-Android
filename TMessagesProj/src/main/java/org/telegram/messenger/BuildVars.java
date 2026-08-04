f381d3f9d94173eecdc4d7eb9a90bcd9ff208e84
halyrds1-alt <halyrds1-alt@users.noreply.github.com>
2026-08-04 10:38:35 +0300

    InfoGram v2.2 - Own API_ID, custom logo, fixed XML errors

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -25,9 +25,10 @@ public class BuildVars {
     public static boolean CHECK_UPDATES = true;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static String BUILD_VERSION_STRING = BuildConfig.BUILD_VERSION_STRING;
-
-    public static int APP_ID = 6;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static 
+int APP_ID = 31759422;
+    public static 
+String APP_HASH = "dd3d4b558b40b5c7e0ef513aeef8bd9f";
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
     public static String SAFETYNET_KEY = "AIzaSyDqt8P-7F7CPCseMkOiVRgb1LY8RN1bvH8";
